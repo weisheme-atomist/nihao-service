525e0bd298385231288a242b0087bc7f3e616d0a
weisheme-atomist <42133380+weisheme-atomist@users.noreply.github.com>
2018-08-07 04:41:03 +0000

    Initial commit from Atomist

---
+++ README.md
@@ -0,0 +1,16 @@
+# nihao-service
+my new project
+
+Based on seed project `johnsonr:flux-flix-service`
+
+## Running the Project
+
+First install Mongo db if you don't have it already.
+
+Then, start the server with:
+
+```
+mvn spring-boot:run
+```
+
+Test by visiting `http://localhost:8080/movies`
---
+++ src/test/kotlin/nihao/NihaoServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package nihao
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class NihaoServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
